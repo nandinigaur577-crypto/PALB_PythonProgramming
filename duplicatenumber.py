01a31273eb86da8bb6d8bc89c0987184c49aa892
nandini gaur <nandinigaur577@gmail.com>
2026-02-12 14:47:49 +0530

    Create duplicatenumber.py

---
+++ duplicatenumber.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def findDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        # Phase 1: Detect cycle
+        slow = nums[0]
+        fast = nums[0]
+        
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+            
+            if slow == fast:
+                break
+        slow = nums[0]
+        
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+        
+        return slow
