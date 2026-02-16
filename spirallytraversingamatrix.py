7abd29394369399bd7578381834bb652e70743e1
nandini gaur <nandinigaur577@gmail.com>
2026-02-16 23:48:26 +0530

    Create spirallytraversingamatrix.py

---
+++ spirallytraversingamatrix.py
@@ -0,0 +1,35 @@
+class Solution:
+    def spirallyTraverse(self, mat):
+        result = []
+        
+        if not mat:
+            return result
+        
+        n = len(mat)
+        m = len(mat[0])
+        
+        top = 0
+        bottom = n - 1
+        left = 0
+        right = m - 1
+        
+        while top <= bottom and left <= right:
+            for i in range(left, right + 1):
+                result.append(mat[top][i])
+            top += 1
+            for i in range(top, bottom + 1):
+                result.append(mat[i][right])
+            right -= 1
+            if top <= bottom:
+                for i in range(right, left - 1, -1):
+                    result.append(mat[bottom][i])
+                bottom -= 1
+            if left <= right:
+                for i in range(bottom, top - 1, -1):
+                    result.append(mat[i][left])
+                left += 1
+        
+        return result
+
+       
+            
